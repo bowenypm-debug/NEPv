f010a84622b12709ea68afd958a833b183206cb8
Boweny <bowenypm@gmail.com>
2026-05-23 21:04:29 +0800

    Update 1_Examples_and_Solvers.py

--- pages/1_Examples_and_Solvers.py
+++ pages/1_Examples_and_Solvers.py
@@ -255,5 +255,5 @@ with col_footer1:
     st.write("Page 2 of 2")
 
 with col_footer2:
-    if st.button("↩️ Introduction", use_container_width=True):
-        st.switch_page("Introduction.py")
+    if st.button("➡️ Applications and Pitfalls", use_container_width=True):
+        st.switch_page("2_Applications_and_Pitfalls.py")
